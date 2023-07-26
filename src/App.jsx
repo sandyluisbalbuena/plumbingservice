acca43e4a7b2237356bd9b7a24b39160de169df3
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-07-27 01:27:07 +0800

    wew

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Notfound from './pages/Notfound'
@@ -7,22 +6,19 @@ import Services from './pages/Services'
 import Service from './pages/Service'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <BrowserRouter basename='/'>
-				<Routes>
-					<Route element={<Template />}>
-						<Route path='/' element={<Home />}/>
-						<Route path='/services' element={<Services />}/>
-						<Route path='/services/:service' element={<Service />}/>
-					</Route>
-					<Route path='*' element={<Notfound />}/>
-				</Routes>
-			</BrowserRouter>
-    </>
-  )
+	return (
+		<BrowserRouter basename='/'>
+			<Routes>
+				<Route element={<Template />}>
+					<Route path='/' element={<Home />}/>
+					<Route path='/services' element={<Services />}/>
+					<Route path='/services/:service' element={<Service />}/>
+				</Route>
+				<Route path='*' element={<Notfound />}/>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
